3f0109add133064da55f1808a901aa95df823599
Olha Millionna <olhmil@alu.edu.gva.es>
2024-06-26 15:59:55 +0200

    .

--- API REST/schemas/alimentos.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const AlimentoSchema = new Schema({
-  nombre: { type: String, required: true },
-  creado: { type: Schema.Types.ObjectId, ref: 'Nutricionista', required: true }
-});
-
-module.exports = mongoose.model('Alimento', AlimentoSchema);
